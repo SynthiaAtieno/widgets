6bc11fb6029ba8a4a1ba0e1acdfc849cb2939c2a
SynthiaAtieno <ondengsynthia222@gmail.com>
2023-06-08 14:19:51 +0300

    tried to create a responsive ui in flutter

---
+++ lib/widgets/login_field.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:widgets/palletes.dart';
+
+class LoginField extends StatelessWidget {
+  final String hintText;
+  const LoginField({Key? key, required this.hintText}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+        constraints: BoxConstraints(
+        maxWidth: MediaQuery.of(context).size.width < 769 ? 300 : 800,
+    ),
+    child: TextFormField(
+    decoration: InputDecoration(
+    enabledBorder: OutlineInputBorder(
+    borderSide: const BorderSide(
+    color: Pallete.borderColor,
+    width: 3
+    ),
+    borderRadius: BorderRadius.circular(10)
+    ),
+    focusedBorder: OutlineInputBorder(
+    borderSide: const BorderSide(
+    color: Pallete.gradient1,
+    width: 3
+    ),
+    borderRadius: BorderRadius.circular(10)
+    ),
+      hintText: hintText,
+    ),
+    ),
+    );
+  }
+}
