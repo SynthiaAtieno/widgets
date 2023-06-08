11ac8ecec2ff381884551d59e9a2ab7371c9ff71
SynthiaAtieno <ondengsynthia222@gmail.com>
2023-06-08 14:20:14 +0300

    tried to create a responsive ui in flutter

---
+++ lib/login_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:widgets/widgets/button.dart';
+import 'package:widgets/widgets/login_field.dart';
+import 'package:widgets/widgets/signin_button.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Image.asset("assets/images/signin_balls.png"),
+                const Text(
+                  "Sign In.",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const MyButton(
+                  iconPath: "assets/svg/g_logo.svg",
+                  label: "Continue with google",
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const MyButton(
+                  iconPath: "assets/svg/f_logo.svg",
+                  label: 'Continue with facebook',
+                  horizontalPadding: 80,
+                ),
+                const SizedBox(height: 15),
+                Text(
+                  "or",
+                  style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width > 769 &&
+                              MediaQuery.of(context).size.height > 1024
+                          ? 24
+                          : 24),
+                ),
+                const SizedBox(height: 15),
+                const LoginField(hintText: "Email"),
+                const SizedBox(height: 15),
+                const LoginField(hintText: "Password"),
+                const SizedBox(height: 20),
+                const GradientButton()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
